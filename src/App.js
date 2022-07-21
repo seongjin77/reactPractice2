0600eaca7621fab9c6d57e07f4bcde1121d616ab
seongjin77 <kssj9056@gmail.com>
2022-07-21 21:36:15 +0900

    연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Modal from './components/Modal';
 import Todo from './components/Todo';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return <div>
     <h1>my Todos</h1>
-    <Todo/>
+    <Todo text='Learn React'/>
+    <Modal/>
+    <Backdrop/>
     </div>;
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,14 @@
-function Todo() {
+function Todo(props) {
+
+  function deleteHandler(){
+    console.log('clicked!')
+  }
+
   return (
     <div className="card">
-      <h2>TITLE</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
